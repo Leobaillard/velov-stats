fc245ba3e361bd8f4b2cca066bf2d7b056e43b2c
Léopold Baillard <leobaillard@leoserveur.org>
2015-10-21 23:27:12 +0200

    First working version with statsd

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # velov-stats
 Getting Velo'V stations metrics for graphing purposes
+
+Fetches data from JCDecaux developer API and pushes them to StatsD/Graphite
+
+## Requirements
+ - Python 2/3
+ - pystatsd: https://github.com/jsocol/pystatsd
+
+## Configuration
+Create a `config.json` next to the Python script containing these values:
+```json
+{
+	"jcd_apikey": "",
+	"jcd_apiurl": "https://api.jcdecaux.com/vls/v1/",
+	"jcd_contract": "",
+	"jcd_stations": [],
+	
+	"graphite_host": "",
+	"graphite_node": "",
+	"statsd_host": "",
+	"statsd_port": 8125,
+	
+	"measure_interval": 60
+}
+
+```
--- vstats.py
+++ vstats.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+import sys
 import requests
+import socket
 import json
+import time
 import pprint
 
-JCD_APIKEY      = ""
-JCD_APIURL      = "https://api.jcdecaux.com/vls/v1/"
-JCD_CONTRACT    = "Lyon"
-JCD_STATIONS    = [6035,6036,6037,6041,6042,6043]
+from statsd import StatsClient
+
+try:
+    with open('./config.json') as config_file:
+        config = json.load(config_file)
+except:
+    print("Can't load config file.")
+    raise
+    exit(1)
 
 def get_stations_data():
     stations_data = {}
-    for station in JCD_STATIONS:
+    for station in config['jcd_stations']:
+        print("Getting station #"+str(station))
         station_json = get_station_data(station)
-        stations_data[str(station)] = {}
-        stations_data[str(station)]['total'] = station_json['bike_stands']
-        stations_data[str(station)]['bikes'] = station_json['available_bike_stands']
-        stations_data[str(station)]['free'] = station_json['available_bikes']
+        stations_data[station] = {}
+        stations_data[station]['bike_stands'] = station_json.get('bike_stands', 0)
+        stations_data[station]['available_bike_stands'] = station_json.get('available_bike_stands', 0)
+        stations_data[station]['available_bikes'] = station_json.get('available_bikes', 0)
 
     return stations_data
 
@@ -25,12 +35,46 @@ def get_station_data(station_id):
     return result
 
 def get_api_call(api_func):
-    api_url = JCD_APIURL + api_func + "?apiKey=" + JCD_APIKEY + "&contract=" + JCD_CONTRACT
+    api_url = config['jcd_apiurl'] + api_func + "?apiKey=" + config['jcd_apikey'] + "&contract=" + config['jcd_contract']
     data = requests.get(api_url)
     if data.status_code == 200:
         return data.json()
     else:
-        return "{}"
+        print("Error fetching data, creating graphite event")
+        create_graphite_event("Error fetching API data (" + api_func +")", ["error"])
+        return json.loads("{}")
+
+def openstatsd():
+    return StatsClient(host=socket.gethostbyname(config['statsd_host']),
+                     port=config['statsd_port'],
+                     prefix=config['graphite_node'])
+
+def create_graphite_event(event_description, tags):
+  required_data = "Event from velov stats"
+  tags_string = " ".join(str(x) for x in tags)  # Since you will pass an array but graphite expects multi tags like "a,b,c" or "a b c"
+  event = {"what": event_description, "tags": tags_string, "data": required_data}
+  try:
+    requests.post("http://"+config['graphite_host']+"/events/", data=json.dumps(event), timeout=15, verify=False)
+  except Exception as exc:
+    print('Error while creating graphite event:', exc, file=sys.stderr)
+
+def main():
+    statsd = openstatsd()
+
+    while(True):
+        stations_data = get_stations_data()
+        for station_id, station_data in stations_data.items():
+            pprint.pprint(str(station_id) + ": "+str(station_data))
+            statsd.gauge(str(station_id) + '.bike_stands', station_data['bike_stands'])
+            statsd.gauge(str(station_id) + '.available_bike_stands', station_data['available_bike_stands'])
+            statsd.gauge(str(station_id) + '.available_bikes', station_data['available_bikes'])
+
+        # Sleep until next iteration
+        print("Waiting "+ str(config['measure_interval']) + "s until next iteration...")
+        time.sleep(config['measure_interval'])
 
 if __name__ == '__main__':
-    pprint.pprint(get_stations_data())
+    main()
+    #pprint.pprint(get_stations_data())
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
